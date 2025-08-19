47800dc6b930def2bf42a039a842dffcf335c3fa
Pipo2410 <Pipo2410@gmail.com>
2025-08-19 22:26:48 +0100

    03: Clerk authentication

--- packages/backend/convex/users.ts
+++ packages/backend/convex/users.ts
@@ -13,6 +13,12 @@ export const getMany = query({
 export const add = mutation({
   args: {},
   handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity()
+
+    if (identity === null) {
+      throw new Error('User not authenticated')
+    }
+
     const userId = await ctx.db.insert('users', { name: 'Peter' })
 
     return userId
