e920fdf95a51f608aefd213f61673708abdb7830
Pipo2410 <Pipo2410@gmail.com>
2025-08-23 19:11:41 +0100

    10: Widget session

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -2,6 +2,7 @@
 
 import WidgetFooter from '@/modules/widget/components/widget-footer'
 import WidgetHeader from '@/modules/widget/components/widget-header'
+import { WidgetAuthScreen } from '@/modules/widget/ui/screens/widget-auth-screen'
 
 interface Props {
   organizationId: string
@@ -10,14 +11,8 @@ interface Props {
 export const WidgetView = ({ organizationId }: Props) => {
   return (
     <main className='min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted'>
-      <WidgetHeader>
-        <div className='flex flex-col justify-between gap-y-2 px-2 py-6 font-semibold'>
-          <p className=' text-3xl'>Hi There! 👋</p>
-          <p className='text-lg'>How can we help you today?</p>
-        </div>
-      </WidgetHeader>
-      <div className='flex flex-1'>Widget View: {organizationId}</div>
-      <WidgetFooter />
+      <WidgetAuthScreen />
+      {/* <WidgetFooter /> */}
     </main>
   )
 }
