66e2c7db07fe0b566626862e11b59169fcab121e
Pipo2410 <Pipo2410@gmail.com>
2025-08-24 18:44:00 +0100

    13: Conversations

---
+++ apps/widget/modules/widget/ui/screens/widget-selection-screen.tsx
@@ -0,0 +1,87 @@
+'use client'
+
+import React, { useState } from 'react'
+import { WidgetHeader } from '@/modules/widget/ui/components/widget-header'
+import { Button } from '@workspace/ui/components/button'
+import { ChevronRightIcon, MessageSquareTextIcon } from 'lucide-react'
+import { useSetAtom, useAtomValue } from 'jotai'
+import {
+  contactSessionIdAtomFamily,
+  conversationIdAtom,
+  errorMessageAtom,
+  organizationIdAtom,
+  screenAtom,
+} from '../../atoms/widget-atoms'
+import { api } from '@workspace/backend/_generated/api'
+import { useMutation } from 'convex/react'
+import { ConvexError } from 'convex/values'
+
+export const WidgetSelectionScreen = () => {
+  const setScreen = useSetAtom(screenAtom)
+  const setErrorMessage = useSetAtom(errorMessageAtom)
+  const setConversationId = useSetAtom(conversationIdAtom)
+
+  const organizationId = useAtomValue(organizationIdAtom)
+  const contactSessionId = useAtomValue(
+    contactSessionIdAtomFamily(organizationId || '')
+  )
+
+  const createConversation = useMutation(api.public.conversations.create)
+  const [isPending, setIsPending] = useState(false)
+  const handleNewConversation = async () => {
+    if (!organizationId) {
+      setScreen('error')
+      setErrorMessage('Missing organization ID')
+      return
+    }
+
+    if (!contactSessionId) {
+      setScreen('auth')
+      return
+    }
+
+    setIsPending(true)
+    try {
+      const conversationId = await createConversation({
+        contactSessionId,
+        organizationId,
+      })
+      setConversationId(conversationId)
+      setScreen('chat')
+    } catch (error) {
+      // Check whether the error is an application error
+      error instanceof ConvexError
+        ? setErrorMessage(error.message)
+        : console.error('Unexpected error occurred', error)
+
+      setScreen('auth')
+    } finally {
+      setIsPending(false)
+    }
+  }
+
+  return (
+    <>
+      <WidgetHeader>
+        <div className='flex flex-col justify-between gap-y-2 px-2 py-6 font-semibold'>
+          <p className=' text-3xl'>Hi There! 👋</p>
+          <p className='text-lg'>Let&apos;s get you started!</p>
+        </div>
+      </WidgetHeader>
+      <div className='flex flex-1 flex-col gap-y-4 p-4 overflow-y-auto'>
+        <Button
+          className='h-16 w-full justify-between'
+          variant='outline'
+          onClick={handleNewConversation}
+          disabled={isPending}
+        >
+          <div className='flex items-center gap-x-2'>
+            <MessageSquareTextIcon className='size-4' />
+            <span>Start chat</span>
+          </div>
+          <ChevronRightIcon />
+        </Button>
+      </div>
+    </>
+  )
+}
