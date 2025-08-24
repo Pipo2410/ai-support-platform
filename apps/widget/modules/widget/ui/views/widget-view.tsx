b1efc5e757aeb5218df9b51df399c31009519f00
Pipo2410 <Pipo2410@gmail.com>
2025-08-24 11:57:58 +0100

    12: Widget loading

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -3,16 +3,18 @@
 import { useAtomValue } from 'jotai'
 import { WidgetAuthScreen } from '@/modules/widget/ui/screens/widget-auth-screen'
 import { screenAtom } from '@/modules/widget/atoms/widget-atoms'
+import { WidgetErrorScreen } from '@/modules/widget/ui/screens/widget-error-screen'
+import { WidgetLoadingScreen } from '@/modules/widget/ui/screens/widget-loading-screen'
 
 interface Props {
-  organizationId: string
+  organizationId: string | null
 }
 
 export const WidgetView = ({ organizationId }: Props) => {
   const screen = useAtomValue(screenAtom)
   const screenComponents = {
-    error: <p>Todo: Error</p>,
-    loading: <p>Todo: Loading</p>,
+    loading: <WidgetLoadingScreen organizationId={organizationId} />,
+    error: <WidgetErrorScreen />,
     auth: <WidgetAuthScreen />,
     voice: <p>Todo: Voice</p>,
     inbox: <p>Todo: Inbox</p>,
