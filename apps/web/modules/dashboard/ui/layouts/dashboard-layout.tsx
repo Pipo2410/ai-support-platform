959f4a5782ab81c3947bee95cd415ddc98b0ee88
Pipo2410 <37326909+Pipo2410@users.noreply.github.com>
2025-10-22 07:45:58 +0100

    18: Dashboard chat

--- apps/web/modules/dashboard/ui/layouts/dashboard-layout.tsx
+++ apps/web/modules/dashboard/ui/layouts/dashboard-layout.tsx
@@ -4,7 +4,9 @@ import { AuthGuard } from '@/modules/auth/ui/components/auth-guard'
 import { OrganizationGuard } from '@/modules/auth/ui/components/organization-guard'
 import { SidebarProvider } from '@workspace/ui/components/sidebar'
 import { DashboardSidebar } from '@/modules/dashboard/ui/components/dashboard-sidebar'
+import { Provider } from 'jotai'
 
+// here to fix dev bug
 const SIDEBAR_COOKIE_NAME = 'sidebar_state'
 
 export const DashboardLayout = async ({
@@ -18,10 +20,12 @@ export const DashboardLayout = async ({
   return (
     <AuthGuard>
       <OrganizationGuard>
-        <SidebarProvider defaultOpen={defaultOpen}>
-          <DashboardSidebar />
-          <main className='flex flex-1 flex-col'>{children}</main>
-        </SidebarProvider>
+        <Provider>
+          <SidebarProvider defaultOpen={defaultOpen}>
+            <DashboardSidebar />
+            <main className='flex flex-1 flex-col'>{children}</main>
+          </SidebarProvider>
+        </Provider>
       </OrganizationGuard>
     </AuthGuard>
   )
