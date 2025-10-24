29a89cbea824c8f474bae39428eaaa03a051b3b3
Pipo2410 <Pipo2410@gmail.com>
2025-10-24 23:32:06 +0100

    19: ai tool calling

--- packages/backend/convex/system/ai/agents/supportAgent.ts
+++ packages/backend/convex/system/ai/agents/supportAgent.ts
@@ -3,6 +3,6 @@ import { Agent } from '@convex-dev/agent'
 import { components } from '../../../_generated/api'
 
 export const supportAgent = new Agent(components.agent, {
-  chat: openai.chat('gpt-4o-mini'),
-  instructions: 'You are a customer support agent',
+  chat: openai.chat('gpt-5-nano'),
+  instructions: `You are a customer support agent. Use "resolveConversation" tool when user expresses finalization of the conversation. Use "escalateConversation" tool when user expresses frustration or requests a human explicitly.`,
 })
