dc59ea40f959c1b21d21620ff830a7ada5e0e646
Pipo2410 <Pipo2410@gmail.com>
2025-11-26 08:38:38 +0000

    34: Embed script

---
+++ apps/embed/config.ts
@@ -0,0 +1,5 @@
+export const EMBED_CONFIG = {
+  WIDGET_URL: import.meta.env.VITE_WIDGET_URL || 'http://localhost:3001',
+  DEFAULT_ORG_ID: 'org_32nHSjbAzt20udaTLyVOIgjYLPD',
+  DEFAULT_POSITION: 'bottom-right' as const,
+}
