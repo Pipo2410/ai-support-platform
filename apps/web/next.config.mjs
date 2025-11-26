2dcac4b41ee00e5f9b111d528c04bc4de11466a8
Pipo2410 <37326909+Pipo2410@users.noreply.github.com>
2025-11-26 09:54:14 +0000

    35: Deployment

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,17 +1,26 @@
-import {withSentryConfig} from "@sentry/nextjs";
+import { withSentryConfig } from '@sentry/nextjs'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@workspace/ui"],
-  devIndicators: false
+  transpilePackages: ['@workspace/ui'],
+  devIndicators: false,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/conversations',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-  org: "none-7k1",
+  org: 'none-7k1',
 
-  project: "ai-support-platform",
+  project: 'ai-support-platform',
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
@@ -26,7 +35,7 @@ export default withSentryConfig(nextConfig, {
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
   // side errors will fail.
-  tunnelRoute: "/monitoring",
+  tunnelRoute: '/monitoring',
 
   // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
@@ -36,4 +45,4 @@ export default withSentryConfig(nextConfig, {
   // https://docs.sentry.io/product/crons/
   // https://vercel.com/docs/cron-jobs
   automaticVercelMonitors: true,
-});
+})
